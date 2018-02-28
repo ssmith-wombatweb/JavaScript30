184c5013f63df37753f6ddbcc22e2894195c9290
ssmith-wombatweb <ssmith@wombatweb.us>
2018-02-27 16:37:00 -0800

    Fixed issues where all hands moved each second.

--- 02 - JS and CSS Clock/index.js
+++ 02 - JS and CSS Clock/index.js
@@ -2,35 +2,49 @@ const hourHand = document.querySelector('.hand.hour-hand');
 const minuteHand = document.querySelector('.hand.min-hand');
 const secondHand = document.querySelector('.hand.second-hand');
 
-const hourTransition = window.getComputedStyle(hourHand).transition;
-
-function setRotation(increment, segments) {
-  return `rotate(${(increment * (360 / segments)) + 90}deg)`;
+function retrieveRotation(transform) {
+  if (transform === '') return null;
+  const firstParenIndex = transform.indexOf('(');
+  const degIndex = transform.indexOf('deg');
+  const rotationText = transform.slice(firstParenIndex + 1, degIndex);
+  const rotation = parseInt(rotationText, 10);
+  return rotation;
 }
 
-function transitionState(increment, element, defaultTransition) {
-  if (increment === 0) {
-    element.style.transition = 'none';
-  } else {
-    element.style.transition = defaultTransition;
+function setRotation(increment, segments, transform) {
+  const rotation = retrieveRotation(transform);
+  if (!rotation) return `rotate(${(increment * (360 / segments)) + 90}deg)`;
+  return `rotate(${rotation + (360 / segments)}deg)`;
+}
+
+function initClock(hourH, minuteH, secondH) {
+  const time = new Date();
+  const hour = time.getHours();
+  const min = time.getMinutes();
+  const sec = time.getSeconds();
+
+  secondH.style.transform = setRotation(sec, 60, secondH.style.transform);
+  minuteH.style.transform = setRotation(min, 60, minuteH.style.transform);
+  hourH.style.transform = setRotation(hour, 12, hourH.style.transform);
+}
+
+function updateClock(hourH, minuteH, secondH) {
+  const time = new Date();
+  const hour = time.getHours();
+  const min = time.getMinutes();
+  const sec = time.getSeconds();
+
+  secondH.style.transform = setRotation(sec, 60, secondH.style.transform);
+
+  if (sec === 0) {
+    minuteH.style.transform = setRotation(min, 60, minuteH.style.transform);
+  }
+
+  if (min === 0) {
+    hourH.style.transform = setRotation(hour, 12, hourH.style.transform);
   }
 }
 
-function updateClock(hourH, minuteH, secondH, defaultTransition) {
-  const time = new Date();
-  const hour = time.getHours();
-  const minute = time.getMinutes();
-  const second = time.getSeconds();
+document.onload = initClock(hourHand, minuteHand, secondHand);
 
-  transitionState(hour, hourH, defaultTransition);
-  transitionState(minute, minuteH, defaultTransition);
-  transitionState(second, secondH, defaultTransition);
-
-  hourH.style.transform = setRotation(hour, 12);
-  minuteH.style.transform = setRotation(minute, 60);
-  secondH.style.transform = setRotation(second, 60);
-}
-
-document.onload = updateClock(hourHand, minuteHand, secondHand, hourTransition);
-
-setInterval(() => updateClock(hourHand, minuteHand, secondHand, hourTransition), 1000);
+setInterval(() => updateClock(hourHand, minuteHand, secondHand), 1000);
