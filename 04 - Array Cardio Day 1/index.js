ac5c5b4f765eabd1dc3d509fa67db475b7693ac8
ssmith-wombatweb <ssmith@wombatweb.us>
2018-02-28 18:46:18 -0800

    Updated with a few tweaks based on Wes Boss examples.

--- 04 - Array Cardio Day 1/index.js
+++ 04 - Array Cardio Day 1/index.js
@@ -86,24 +86,19 @@ const people = ['Blake, William', 'Beck, Glenn', 'Becker, Carl', 'Beckett, Samue
 const sixteenthCenturyInventors = inventors.filter(({ year }) =>
   year >= 1500 && year < 1600);
 console.log('INVENTORS FROM THE SIXTEENTH CENTURY');
-console.log(sixteenthCenturyInventors);
+console.table(sixteenthCenturyInventors);
 
 // Array.prototype.map()
 // 2. Give us an array of the inventors' first and last names
-const inventorNames = inventors.map(({ first, last }) => (
-  {
-    first,
-    last,
-  }
-));
+const inventorNames = inventors.map(({ first, last }) => `${first} ${last}`);
 console.log('NAMES OF THE INVENTORS');
-console.log(inventorNames);
+console.table(inventorNames);
 
 // Array.prototype.sort()
 // 3. Sort the inventors by birthdate, oldest to youngest
 const inventorByBirthYear = inventors.sort((a, b) => a.year - b.year);
 console.log('INVENTORS SORTED BY BIRTH YEAR');
-console.log(inventorByBirthYear);
+console.table(inventorByBirthYear);
 
 // Array.prototype.reduce()
 // 4. How many years did all the inventors live?
@@ -116,36 +111,36 @@ console.log(totalInventorAge);
 const inventorByAge = inventors.sort((a, b) =>
   (a.passed - a.year) - (b.passed - b.year));
 console.log('INVENTORS BY AGE');
-console.log(inventorByAge);
+console.table(inventorByAge);
 
 // 6. create a list of Boulevards in Paris that contain 'de' anywhere in the name
 // https://en.wikipedia.org/wiki/Category:Boulevards_in_Paris
+// Done in the console on the page.
+// const category = document.querySelector('.mw-category');
+// const links = [...category.querySelectorAll('a')];
 
+// const de = links
+//   .map(link => link.textContent)
+//   .filter(streetName => streetName.includes('de'));
 
 // 7. sort Exercise
 // Sort the people alphabetically by last name
 const peopleByLastName = people.sort();
 console.log('PEOPLE SORTED BY LAST NAME');
-console.log(peopleByLastName);
+console.table(peopleByLastName);
 
 // 8. Reduce Exercise
 // Sum up the instances of each of these
 const data = ['car', 'car', 'truck', 'truck', 'bike', 'walk', 'car', 'van', 'bike', 'walk', 'car', 'van', 'car', 'truck'];
-const instancesOfData = data.reduce(
-  (instances, item) => {
-    const updatedInstances = instances;
-    const numInstance = updatedInstances[item] + 1;
-    updatedInstances[item] = numInstance;
-    return updatedInstances;
-  },
-  {
-    car: 0,
-    truck: 0,
-    bike: 0,
-    walk: 0,
-    van: 0,
-  },
-);
+
+const instancesOfData = data.reduce((obj, item) => {
+  const updatedObj = obj;
+  if (!updatedObj[item]) {
+    updatedObj[item] = 0;
+  }
+  updatedObj[item] += 1;
+  return updatedObj;
+}, {});
 
 console.log('NUMBER OF EACH DATA ITEM');
-console.log(instancesOfData);
+console.table(instancesOfData);
