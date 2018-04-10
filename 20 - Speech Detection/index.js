56f8bef3fa6550e8060a1872647c1495cfc8e012
ssmith-wombatweb <ssmith@wombatweb.us>
2018-04-09 17:07:56 -0700

    Made some emoji replacements for the kids.

--- 20 - Speech Detection/index.js
+++ 20 - Speech Detection/index.js
@@ -1,7 +1,7 @@
 
 window.SpeechRecognition = window.SpeechRecognition || window.webkitSpeechRecognition;
 
-const recognition = new SpeechRecognition();
+const recognition = new window.SpeechRecognition();
 recognition.interimResults = true;
 
 let p = document.createElement('p');
@@ -9,10 +9,32 @@ const words = document.querySelector('.words');
 words.appendChild(p);
 
 recognition.addEventListener('result', (e) => {
-  const transcript = [...e.results]
+  let transcript = [...e.results]
     .map(result => result[0])
     .map(result => result.transcript)
     .join('');
+
+  transcript = transcript
+    .replace(/poop/gi, '💩')
+    // .replace(/love/gi, '😍')
+    .replace(/starry eyed/gi, '🤩')
+    .replace(/puke/gi, '🤮')
+    .replace(/giant/gi, '👹')
+    .replace(/baby/gi, '👶')
+    .replace(/hair guy/gi, '💂')
+    .replace(/angel/gi, '👼')
+    .replace(/wizard/gi, '🧙‍♂')
+    .replace(/vampire/gi, '🧛‍♂️')
+    .replace(/zombie/gi, '🧟')
+    .replace(/surfer/gi, '🏄')
+    .replace(/juggling man/gi, '🤹')
+    .replace(/live long and prosper/gi, '🖖')
+    .replace(/Hi-5|high five/gi, '🤚')
+    .replace(/fist bump/gi, '🤛')
+    .replace(/fox/gi, '🦊')
+    .replace(/dog/gi, '🐕')
+    .replace(/i love you/gi, '🤟');
+
   p.textContent = transcript;
 
   if (e.results[0].isFinal) {
