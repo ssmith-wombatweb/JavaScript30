028875f906b682142fb2e5568b9dfe7cfed23e2a
ssmith-wombatweb <ssmith@wombatweb.us>
2018-04-10 19:56:54 -0700

    Made some slight changes after watching Wes's solution.

--- 21 - Geolocation/index.js
+++ 21 - Geolocation/index.js
@@ -1,10 +1,11 @@
 const arrow = document.querySelector('.arrow');
-const speed = document.querySelector('.speed');
+const speedValue = document.querySelector('.speed-value');
 
-navigator.geolocation.watchPosition((data) => {
-  const { heading, speed: kmph } = data.coords;
-  arrow.style.transform = `rotate(${heading}deg)`;
-  speed.querySelector('.speed-value').textContent = kmph.toFixed(2);
-},
-err => console.error(err),
+navigator.geolocation.watchPosition(
+  (data) => {
+    const { heading, speed: kmph } = data.coords;
+    arrow.style.transform = `rotate(${heading}deg)`;
+    speedValue.textContent = kmph.toFixed(2);
+  },
+  err => console.error(err),
 );
