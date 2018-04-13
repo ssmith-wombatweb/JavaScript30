fc2b32258d610e0ea2b4e443424560aea3725cb2
ssmith-wombatweb <ssmith@wombatweb.us>
2018-04-13 13:01:27 -0700

    Created a follow along dropdown menu.

---
+++ 26 - Stripe Follow Along Nav/index.js
@@ -0,0 +1,34 @@
+const triggers = document.querySelectorAll('.cool > li');
+const background = document.querySelector('.dropdownBackground');
+const nav = document.querySelector('.top');
+
+function handleEnter() {
+  this.classList.add('trigger-enter');
+  setTimeout(() => this.classList.contains('trigger-enter') && this.classList.add('trigger-enter-active'), 150);
+  background.classList.add('open');
+
+  const dropdown = this.querySelector('.dropdown');
+
+  const dropdownCoords = dropdown.getBoundingClientRect();
+  const navCoords = nav.getBoundingClientRect();
+
+  const coords = {
+    left: dropdownCoords.left - navCoords.left,
+    top: dropdownCoords.top - navCoords.top,
+    width: dropdownCoords.width,
+    height: dropdownCoords.height,
+  };
+
+  background.style.setProperty('width', `${coords.width}px`);
+  background.style.setProperty('height', `${coords.height}px`);
+  background.style.setProperty('transform', `translate(${coords.left}px, ${coords.top}px`);
+}
+
+function handleLeave() {
+  this.classList.remove('trigger-enter-active');
+  setTimeout(() => this.classList.remove('trigger-enter'), 500);
+  background.classList.remove('open');
+}
+
+triggers.forEach(trigger => trigger.addEventListener('mouseenter', handleEnter));
+triggers.forEach(trigger => trigger.addEventListener('mouseleave', handleLeave));
