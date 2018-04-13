5c0fd5d35f0f8f6332db9acf4126c9e502a22440
ssmith-wombatweb <ssmith@wombatweb.us>
2018-04-13 16:32:37 -0700

    Modified the video speed solution to ensure that it never is too small for the text.

---
+++ 28 - Video Speed Controller/index.js
@@ -0,0 +1,28 @@
+const speed = document.querySelector('.speed');
+const bar = document.querySelector('.speed-bar');
+const video = document.querySelector('.flex');
+let mouseDown = false;
+const barHeight = bar.offsetHeight;
+
+function updateSpeed(e) {
+  if (!mouseDown) return;
+  const y = e.pageY - this.offsetTop;
+  const speedHeight = this.offsetHeight;
+  const percent = y / speedHeight;
+  const minHeight = parseInt(bar.style.minHeight, 0) / 100;
+  const speedMax = 4;
+  const height = `${Math.round(percent * 100)}%`;
+  const playbackRate = percent < minHeight ? minHeight * speedMax : percent * speedMax;
+  bar.style.height = height;
+  bar.textContent = `${playbackRate.toFixed(2)}x`;
+  video.playbackRate = playbackRate;
+}
+
+function barClicked(e) {
+  mouseDown = true;
+  updateSpeed.call(this, e);
+}
+
+speed.addEventListener('mousedown', barClicked);
+document.body.addEventListener('mouseup', () => { mouseDown = false; });
+speed.addEventListener('mousemove', updateSpeed);
